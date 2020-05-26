11536bddab7dd1e230bfb042ed253be950876945
Teresa Brooks <teresa.brooks@gmail.com>
2020-05-25 22:12:49 -0400

    added modified methods for random forest and todos for spliting data as numpy array

--- biased_random_forest/models/biased_random_forest.py
+++ biased_random_forest/models/biased_random_forest.py
@@ -1,6 +1,7 @@
 from random import seed
 from random import randrange
 
+
 class BiasedRandomForest(object):
     """
     This class implements is an biased random forest, and ensemble method that seeks to mitigate issues caused by class
@@ -35,8 +36,203 @@ class BiasedRandomForest(object):
         self._minimum_sample_size = min_size
         self._sample_ratio = sample_size
 
-    def fit(self, X_data, y_labels):
+    def fit(self, X_train):
+        """
+
+        :param X_train:
+        :return:
+        """
+
         # todo: sort out the minority class + critical version of the training data
         # todo: return combined RF as the model
-        pass
+
+        forest = self._generate_forest(X_train)
+
+        return forest
+
+    @staticmethod
+    def _random_subsample(sample_data, ratio):
+        """
+        Create a random subsample from the dataset with replacement.
+        :param sample:
+        :param ratio:
+        :return:
+        """
+
+        subsample = list()
+        num_samples = round(len(sample_data) * ratio)
+
+        while len(subsample) < num_samples:
+            index = randrange(len(sample_data))
+            subsample.append(sample_data[index])
+
+        return subsample
+
+    @staticmethod
+    def _to_terminal(group):
+        """
+
+        :param group:
+        :return:
+        """
+        outcomes = [row[-1] for row in group]
+        return max(set(outcomes), key=outcomes.count)
+
+    @staticmethod
+    def _gini_index(groups, classes):
+        """
+
+        :param groups:
+        :param classes:
+        :return:
+        """
+
+        # count all samples at split point
+        n_instances = float(sum([len(group) for group in groups]))
+
+        # sum weighted Gini index for each group
+        gini = 0.0
+
+        for group in groups:
+            size = float(len(group))
+
+            # avoid divide by zero
+            if size == 0:
+                continue
+            score = 0.0
+
+            # score the group based on the score for each class
+            for class_val in classes:
+                p = [row[-1] for row in group].count(class_val) / size
+                score += p * p
+
+            # weight the group score by its relative size
+            gini += (1.0 - score) * (size / n_instances)
+
+        return gini
+
+    @staticmethod
+    def _split_feature_value(index, value, dataset):
+        """
+
+        :param value:
+        :param dataset:
+        :return:
+        """
+
+        left, right = list(), list()
+
+        for row in dataset:
+            if row[index] < value:
+                left.append(row)
+            else:
+                right.append(row)
+
+        return left, right
+
+    @staticmethod
+    def _select_best_split_point(data, n_features):
+        """
+        Select the best split point for a dataset
+        :param data:
+        :param n_features:
+        :return:
+        """
+
+        target_class_values = list(set(row[-1] for row in data))
+        best_index, best_value, best_score, best_groups = 999, 999, 999, None
+        features = list()
+
+        while len(features) < n_features:
+            index = randrange(len(data[0]) - 1)
+            if index not in features:
+                features.append(index)
+
+        for index in features:
+            for row in data:
+                groups = BiasedRandomForest._split_feature_value(index, row[index], data)
+                gini = BiasedRandomForest._gini_index(groups, target_class_values)
+
+                if gini < best_score:
+                    best_index = index
+                    best_value = row[index]
+                    best_score = gini
+                    best_groups = groups
+
+        return {'index': best_index, 'value': best_value, 'groups': best_groups}
+
+    def _split_node(self, node, max_depth, min_size, n_features, depth):
+        """
+         Create child splits for a node or make terminal
+        :param node:
+        :param max_depth:
+        :param min_size:
+        :param n_features:
+        :param depth:
+        :return:
+        """
+
+        left, right = node['groups']
+        del (node['groups'])
+
+        # check for a no split
+        if not left or not right:
+            node['left'] = node['right'] = BiasedRandomForest._to_terminal(left + right)
+            return
+
+        # check for maximum depth
+        if depth >= max_depth:
+            node['left'] = BiasedRandomForest._to_terminal(left)
+            node['right'] = BiasedRandomForest._to_terminal(right)
+            return
+
+        # process left child
+        if len(left) <= min_size:
+            node['left'] = BiasedRandomForest._to_terminal(left)
+        else:
+            node['left'] = self._select_best_split_point(left, n_features)
+            self._split_node(node['left'], max_depth, min_size, n_features, depth + 1)
+
+        # process right child
+        if len(right) <= min_size:
+            node['right'] = BiasedRandomForest._to_terminal(right)
+        else:
+            node['right'] = self._select_best_split_point(right, n_features)
+            self._split_node(node['right'], max_depth, min_size, n_features, depth + 1)
+
+    def _build_tree(self, data):
+        """
+        Build decision tree.
+        :param data:
+        :return:
+        """
+
+        initial_depth = 1
+
+        # get root
+        root = BiasedRandomForest._select_best_split_point(data,  self._num_features_for_split)
+
+        # create child splits
+        self._split_node(root, self._maximum_depth, self._minimum_sample_size, self._num_features_for_split, initial_depth)
+
+        return root
+
+    def _generate_forest(self, X_train):
+        """
+        Generates random forest.
+        :param X_train: multidimensional array
+        :return: list of tress (dicts)
+        """
+
+        trees = list()
+
+        for i in range(self._s_forest_size):
+            # get random subsample
+            subsample = BiasedRandomForest._random_subsample(X_train, self._sample_ratio)
+
+            # build tree from subsample
+            tree = self._build_tree(subsample)
+            trees.append(tree)
+
+        return trees
 
--- train.py
+++ train.py
@@ -1,3 +1,44 @@
+from math import sqrt
+from biased_random_forest.utils.preprocess import replace_zero_values, scale_numeric_features
+from biased_random_forest.models.biased_random_forest import BiasedRandomForest
+import pandas as pd
+import os.path
+import numpy
+
+
+def load_csv_as_dataframe(path):
+    """
+    Load csv as pandas dataframe.
+    :param path: csv file path
+    :return: dataframe or None
+    """
+
+    df = None
+
+    if path:
+     df = pd.read_csv(path)
+
+    return df
+
+
+def preprocess_data(df, columns, target_column, array_agg_function='median'):
+    """
+    Preprocess data in preparation for training or inference.
+    :param df: pandas dataframe
+    :param columns: columns with zero values to be replaced
+    :param array_agg_function: numpy array aggregating function (i.e mean, median)
+    :param target_column: string, name of column with target variable value
+    :return: pandas dataframe
+    """
+
+    # replace all columns with zero values with their given aggregating value
+    replace_zero_values(df, columns, array_agg_function)
+
+    # scale features
+    normalized_df = scale_numeric_features(df, target_column)
+
+    return normalized_df
+
 
 if __name__ == "__main__":
 
@@ -5,4 +46,25 @@ if __name__ == "__main__":
     # running predictions for both train and test set (calculate metrics train and test:
     #   precision, recall, AUPRC and AUROC for 10-fold cross-validation.)
 
-    pass
+    # Load pima data set
+    abs_path = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(abs_path, "data/diabetes.csv")
+    pima_dataset_df = load_csv_as_dataframe(path)
+
+    # Note, these columns were identified during quick EDA analysis
+    columns_with_zero_values = ['BloodPressure', 'SkinThickness', 'Insulin', 'BMI']
+
+    # Preprocess data for training and model evaluation
+    pima_target_column = 'Outcome'
+    processed_pima_df = preprocess_data(pima_dataset_df, columns_with_zero_values, pima_target_column)
+
+    print(processed_pima_df.head(5))
+
+    # train RF
+    dataset = processed_pima_df.to_numpy()
+    num_features = int(sqrt(len(dataset[0]) - 1))
+
+    # TODO: split train/test datasets and train model on "train"
+
+    braf = BiasedRandomForest(num_features)
+    model = braf.fit(dataset)
